68a0da7be9622b8c4c0d86822810f4ba05fd69fb
Moasis Antonio <moasis.antonio@gmail.com>
2026-07-29 15:45:10 +0700

    Configure next.config.ts to static export

--- pjn-logistics-web/next.config.ts
+++ pjn-logistics-web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // <-- Tambahkan/ubah baris ini
+  images: {
+    unoptimized: true, // Wajib diaktifkan jika menggunakan output 'export'
+  },
 };
 
 export default nextConfig;
